8f1313ee8fd52d994f68b2ca564b83ed10d4b122
Emmanuel <adiemmanuel10@gmail.com>
2024-04-25 01:31:38 +0000

    first python assignment

---
+++ assign4_2.py
@@ -0,0 +1,4 @@
+first_name = 'Emmanuel'
+middle_name = 'Koi'
+last_name = 'Adi'
+print(first_name+ " " +middle_name+ " " +last_name)
---
+++ assign4_3.py
@@ -0,0 +1,35 @@
+a = 10
+type_a = type(a)
+print(f"The value of a is {a}, and the datatype of a is {type_a}")
+
+b = "10"
+type_b = type(b)
+print(f"The value of b is {b}, and the datatype of b is {type_b}")
+
+c = 10.0
+type_c = type(c)
+print(f"The value of c is {c}, and the datatype of c is {type_c}")
+
+d = True
+type_d = type(d)
+print(f"The value of d is {d}, and the datatype of d is {type_d}")
+
+e = {}
+type_e = type(e)
+print(f"The value of e is {e}, and the datatype of e is {type_e}")
+
+f = 3 + 5j
+type_f = type(f)
+print(f"The value of f is {f}, and the datatype of f is {type_f}")
+
+g = [10, "10", 10.0, [3 + 5j], True]
+type_g = type(g)
+print(f"The value of g is {g}, and the datatype of g is {type_g}")
+
+h = (1,2,3,4)
+type_h = type(h)
+print(f"The value of h is {h}, and the datatype of h is {type_h}")
+
+i = {1,2,3,4}
+type_i = type(i)
+print(f"The value of i is {i}, and the datatype of i is {type_i}")
